319aef4da15443868e950fd4203c1bc9dd38e7e3
Tram Huynh <huynhthidieutram97@gmail.com>
2019-03-13 23:11:19 +0700

    Add Database

---
+++ EM.Database/Schema/Basic/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EM.Database.Schema.Basic
+{
+    public abstract class Table
+    {
+        public DateTime? Created_at { set; get; }
+        public int Created_by { set; get; }
+        public DateTime? Updated_at { set; get; }
+        public int? Updated_by { set; get; }
+        public bool DelFlag { set; get; }
+    }
+    public abstract class TableHaveIdInt : Table
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int Id { set; get; }
+    }
+}
---
+++ EM.Database/Schema/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EM.Database.Schema
+{
+    public class Student
+    {
+    }
+}
