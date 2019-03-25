be8420ffb3fdaac123e59ea8bc407a52ce9995d5
Nguyễn Văn Mẫn <man_gareth@outlook.com.vn>
2019-03-25 10:28:45 +0700

    enable cors

---
+++ EducationManagement/Controllers/Bases/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace EducationManagement.Controllers.Bases
+{
+    [EnableCors(origins: "*", headers:"*", methods:"*")]
+    public class BaseApiController : ApiController
+    {
+    }
+}
