f979930be692d2b8b62a427c5da7eb58a26ac170
Nguyễn Văn Mẫn <man_gareth@outlook.com.vn>
2019-04-02 23:45:29 +0700

    get school information

---
+++ EducationManagement/Controllers/SchoolInformationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using EducationManagement.Controllers.Bases;
+using EducationManagement.Services.Abstractions;
+
+namespace EducationManagement.Controllers
+{
+    [RoutePrefix("api/schoolinformation")]
+    public class SchoolInformationController : BaseApiController
+    {
+        private readonly ISchoolInformationService _schoolInformationService;
+
+        public SchoolInformationController(ISchoolInformationService schoolInformationService)
+        {
+            _schoolInformationService = schoolInformationService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetSchoolInformation()
+        {
+            return Ok(_schoolInformationService.GetSchoolInformation());
+        }
+    }
+}
---
+++ EducationManagement/Services/Implementations/SchoolInformationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using EducationManagement.Dtos.OutputDtos;
+using EducationManagement.Services.Abstractions;
+using EM.Database;
+
+namespace EducationManagement.Services.Implementations
+{
+    public class SchoolInformationService : ISchoolInformationService
+    {
+        private readonly DataContext db = new DataContext();
+
+        public SchoolInformationDto GetSchoolInformation()
+        {
+            var schoolInformationList = db.SchoolInformation;
+
+            if (schoolInformationList == null || !schoolInformationList.Any()) return new SchoolInformationDto();
+
+            return new SchoolInformationDto(schoolInformationList.First());
+        }
+    }
+}
