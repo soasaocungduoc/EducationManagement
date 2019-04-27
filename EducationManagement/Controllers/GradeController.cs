a4303b524e1489a4e0aef13e72493d7ec43b6e8f
Tram Huynh <huynhthidieutram97@gmail.com>
2019-04-27 17:08:21 +0700

    create get grades api

---
+++ EducationManagement/Controllers/GradeController.cs
@@ -0,0 +1,35 @@
+﻿using EducationManagement.Controllers.Bases;
+using EducationManagement.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EducationManagement.Controllers
+{
+    public class GradeController : BaseApiController
+    {
+        private readonly IGradeService _gradeService;
+
+        public GradeController(IGradeService gradeService)
+        {
+            _gradeService = gradeService;
+        }
+
+        [HttpGet]
+        [ActionName("GetGrades")]
+        public IHttpActionResult GetGrades()
+        {
+            try
+            {
+                return Ok(_gradeService.GetGrades());
+            }
+            catch (System.Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+}
---
+++ EducationManagement/Dtos/OutputDtos/GradeResponseDto.cs
@@ -0,0 +1,34 @@
+﻿using EM.Database.Schema;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EducationManagement.Dtos.OutputDtos
+{
+    public class GradeResponseDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public GradeResponseDto()
+        {
+
+        }
+
+        public GradeResponseDto(Grade grade)
+        {
+            Id = grade.Id;
+            Name = grade.Name;
+        }
+
+        public GradeResponseDto(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
