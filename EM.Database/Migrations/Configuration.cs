72810fa7156a5c4e73cb2463bc62c542d9b931e9
Tram Huynh <huynhthidieutram97@gmail.com>
2019-05-02 18:07:44 +0700

    create user database

--- EM.Database/Migrations/Configuration.cs
+++ EM.Database/Migrations/Configuration.cs
@@ -17,93 +17,6 @@ namespace EM.Database.Migrations
         {
             base.Seed(context);
 
-            context.TypeMarks.Add(new TypeMark
-            {
-                Name = "Miệng",
-                Factor = 1
-            });
-
-            context.TypeMarks.Add(new TypeMark
-            {
-                Name = "15 phút",
-                Factor = 1
-            });
-
-            context.TypeMarks.Add(new TypeMark
-            {
-                Name = "1 tiết",
-                Factor = 2
-            });
-
-            context.TypeMarks.Add(new TypeMark
-            {
-                Name = "Học kỳ",
-                Factor = 3
-            });
-
-            context.TypeMarks.Add(new TypeMark
-            {
-                Name = "TBC",
-                Factor = 0
-            });
-
-            context.Conducts.Add(new Conduct
-            {
-                ConductType = "Tốt"
-            });
-            context.Conducts.Add(new Conduct
-            {
-                ConductType = "Khá"
-            });
-            context.Conducts.Add(new Conduct
-            {
-                ConductType = "Trung bình"
-            });
-            context.Conducts.Add(new Conduct
-            {
-                ConductType = "Yếu"
-            });
-
-            context.Conducts.Add(new Conduct
-            {
-                ConductType = "Kém"
-            });
-
-            context.Classifications.Add(new Classification
-            {
-                FromGpa = 8.0,
-                ToGpa = 10.0,
-                Name = "Giỏi"
-            });
-
-            context.Classifications.Add(new Classification
-            {
-                FromGpa = 6.5,
-                ToGpa = 7.9,
-                Name = "Khá"
-            });
-
-            context.Classifications.Add(new Classification
-            {
-                FromGpa = 5.0,
-                ToGpa = 6.4,
-                Name = "Trung bình"
-            });
-
-            context.Classifications.Add(new Classification
-            {
-                FromGpa = 3.5,
-                ToGpa = 4.9,
-                Name = "Yếu"
-            });
-
-            context.Classifications.Add(new Classification
-            {
-                FromGpa = 0.0,
-                ToGpa = 3.4,
-                Name = "Kém"
-            });
-
             //context.Functions.Add(new Function
             //{
             //    ScreenId = 8,
@@ -114,17 +27,17 @@ namespace EM.Database.Migrations
             //    ActionName = "GetTeachingSchedulesByTeacherId",
             //});
 
-            //context.Users.Add(new User
-            //{
-            //    FirstName = "Thông",
-            //    LastName = "Trần Văn",
-            //    Avatar = "https://i.pinimg.com/originals/78/7f/27/787f271113b6fae60a7144dbff2b394a.jpg",
-            //    Gender = true,
-            //    Birthday = new DateTime(1997, 10, 30),
-            //    PhoneNumber = "0789456123",
-            //    Address = "123 Nguyễn Lương Bằng",
-            //    IdentificationNumber = "123456789",
-            //});
+            context.Users.Add(new User
+            {
+                FirstName = "Khoa",
+                LastName = "Trần Văn",
+                Avatar = "https://i.pinimg.com/originals/78/7f/27/787f271113b6fae60a7144dbff2b394a.jpg",
+                Gender = true,
+                Birthday = new DateTime(1997, 10, 30),
+                PhoneNumber = "0789456123",
+                Address = "123 Nguyễn Lương Bằng",
+                IdentificationNumber = "123456789",
+            });
 
             //context.Accounts.Add(new Account
             //{
