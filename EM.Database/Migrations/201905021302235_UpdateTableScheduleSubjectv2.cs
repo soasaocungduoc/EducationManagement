76602cc639ecdb2ea64c4903d67244dc22569aeb
Tram Huynh <huynhthidieutram97@gmail.com>
2019-05-02 20:11:15 +0700

    add semesterId again

---
+++ EM.Database/Migrations/201905021302235_UpdateTableScheduleSubjectv2.cs
@@ -0,0 +1,22 @@
+namespace EM.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateTableScheduleSubjectv2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ScheduleSubject", "SemesterId", c => c.Int(nullable: false, defaultValue: 10));
+            CreateIndex("dbo.ScheduleSubject", "SemesterId");
+            AddForeignKey("dbo.ScheduleSubject", "SemesterId", "dbo.Semester", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ScheduleSubject", "SemesterId", "dbo.Semester");
+            DropIndex("dbo.ScheduleSubject", new[] { "SemesterId" });
+            DropColumn("dbo.ScheduleSubject", "SemesterId");
+        }
+    }
+}
