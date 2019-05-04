75945735478a08388ceac22ecaacbda18d69e83b
Tram Huynh <huynhthidieutram97@gmail.com>
2019-05-04 19:27:59 +0700

    create add schedule subject api (working)

---
+++ EducationManagement/Dtos/InputDtos/ScheduleSubjectDto.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EducationManagement.Dtos.InputDtos
+{
+    public class ScheduleSubjectDto
+    {
+        [Required]
+        [StringLength(200)]
+        [JsonProperty("day")]
+        public int Day { get; set; }
+
+        [Required]
+        [JsonProperty("lesson")]
+        public int Lesson { get; set; }
+
+        [Required]
+        [JsonProperty("subject_id")]
+        public int SubjectId { get; set; }
+
+        [Required]
+        [JsonProperty("teacher_id")]
+        public int TeacherId { get; set; }
+    }
+}
