ef9ead575a99b958b28eea276e6c534c6d2df575
Tram Huynh <huynhthidieutram97@gmail.com>
2019-05-05 11:38:55 +0700

    create for parent data

--- EM.Database/Migrations/Configuration.cs
+++ EM.Database/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
-using EM.Database.Schema;
+﻿using EM.Database.Schema;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Data.Entity.Validation;
 using System.Linq;
@@ -145,6 +146,40 @@ namespace EM.Database.Migrations
             //    ActionName = "GetSlides",
             //});
 
+            //List<string> ten = new List<string>{"An", "Kiều", "Cường", "Đạt", "Hằng", "Uyên", "Giàu", "Hạnh", "Trường", "Khiêm", "Lan", "Minh", "Ngọc", "Anh", "Phong", "Quốc",
+            //    "Nhi", "Sơn", "Phúc", "Quyên", "Quang","My","Vy" };
+            //List<string> ho = new List<string> { "Đinh", "Huỳnh", "Lê", "Trương", "Hoàng", "Trần", "Lý" };
+            //for (int i = 0; i < 22; i++)
+            //{
+            //    context.Users.Add(new User
+            //    {
+            //        FirstName = ten[i],
+            //        LastName = ho[i%7],
+            //        Avatar = "https://i.pinimg.com/originals/78/7f/27/787f271113b6fae60a7144dbff2b394a.jpg",
+            //        Gender = false,
+            //        Birthday = new DateTime(1997, 10, i+1),
+            //        PhoneNumber = "0789456123",
+            //        Address = "123 Nguyen Luong Bang",
+            //        IdentificationNumber = "123456789",
+            //    });
+            //}
+
+            //for (int i = 77; i <= 98; i++)
+            //{
+
+            //    context.Parents.Add(new Parent
+            //    {
+            //        UserId = i
+            //    });
+
+            //    context.Accounts.Add(new Account
+            //    {
+            //        UserId = i,
+            //        GroupId = 5,
+            //        UserName = "parent" + i,
+            //        Password = DatabaseCreation.GetMd5(DatabaseCreation.GetSimpleMd5("123456")),
+            //    });
+            //}
             //context.Users.Add(new User
             //{
             //    FirstName = "Tram",
