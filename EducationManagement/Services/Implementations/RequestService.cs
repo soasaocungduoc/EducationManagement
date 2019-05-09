2d553dfa46d7fcb90875a456e53cd88d3897dd72
Hà Nguyễn <nkh1111202@gmail.com>
2019-05-09 16:26:09 +0700

    get all request

---
+++ EducationManagement/Services/Implementations/RequestService.cs
@@ -0,0 +1,94 @@
+﻿using EducationManagement.Dtos.InputDtos;
+using EducationManagement.Dtos.OutputDtos;
+using EducationManagement.Services.Abstractions;
+using EM.Database;
+using EM.Database.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EducationManagement.Services.Implementations
+{
+    public class RequestService : IRequestService
+    {
+        private readonly DataContext db = new DataContext();
+        
+        public bool Add(RequestDto dto)
+        {
+            if (dto == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                var request = new Notification();
+
+                request.SenderId = dto.SenderId;
+                request.Title = dto.Title;
+                request.Content = dto.Content;
+                request.Type = "request";
+
+                db.Notifications.Add(request);
+                db.SaveChanges();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public RequestResponseDto Get(int requestId)
+        {
+            var requestFromDb = db.Notifications
+                .Include(x => x.Sender)
+                .FirstOrDefault(x => x.Type == "request" && x.Id == requestId && x.DelFlag == false);
+
+            if(requestFromDb == null)
+            {
+                return null;
+            }
+
+            return new RequestResponseDto()
+            {
+                 Id = requestFromDb.Id,
+                 Title = requestFromDb.Title,
+                 Content = requestFromDb.Content,
+                 Sender = new UserResponseDto(requestFromDb.Sender)
+            };
+        }
+
+        
+        public List<RequestResponseDto> GetAll()
+        {
+            var requests = new List<RequestResponseDto>();
+
+            requests = db.Notifications
+                .Include(x => x.Sender)
+                .Where(x => x.Type == "request" && x.DelFlag == false)
+                .Select(x => new RequestResponseDto {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Content = x.Content,
+                    Sender = x.Sender == null? null : new UserResponseDto()
+                    {
+                        Id = x.Sender.Id,
+                        FirstName = x.Sender.FirstName,
+                        LastName = x.Sender.LastName,
+                        Address = x.Sender.Address,
+                        Avatar = x.Sender.Avatar,
+                        Birthday = x.Sender.Birthday,
+                        Gender = x.Sender.Gender,
+                        IdentificationNumber = x.Sender.IdentificationNumber,
+                        PhoneNumber = x.Sender.PhoneNumber
+                    }
+                }).ToList();
+
+            return requests;
+        }
+    }
+}
